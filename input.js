0e8ad2448433cb411ca63114b52c3401dbf5d936
Eric Laursen <ericjlaursen@gmail.com>
2019-12-03 06:36:48 -0500

    more functional

--- input.js
+++ input.js
@@ -1,51 +1,57 @@
 'use strict';
-var inquirer = require('..');
+var inquirer = require('inquirer');
 var chalkPipe = require('chalk-pipe');
 
-var questions = [
+const printResults = answers => console.log(JSON.stringify(answers, null, '  '))
+const transformColor = function(color, answers, flags) {
+  const text = chalkPipe(color)(color);
+  if (flags.isFinal) {
+    return text + '!';
+  }
+
+  return text;
+}
+
+const validatePhoneNumber = function(value) {
+  var pass = value.match(
+    /^([01]{1})?[-.\s]?\(?(\d{3})\)?[-.\s]?(\d{3})[-.\s]?(\d{4})\s?((?:#|ext\.?\s?|x\.?\s?){1}(?:\d+)?)?$/i
+  );
+  if (pass) {
+    return true;
+  }
+
+  return 'Please enter a valid phone number';
+}
+
+const defaultFirstName = () => 'Water'
+const defaultLastName = () => 'Melon'
+
+
+const questions = [
   {
     type: 'input',
     name: 'first_name',
-    message: "What's your first name"
+    message: "What's your first name",
+    default: defaultFirstName
   },
   {
     type: 'input',
     name: 'last_name',
     message: "What's your last name",
-    default: function() {
-      return 'Doe';
-    }
+    default: defaultLastName
   },
   {
     type: 'input',
     name: 'fav_color',
     message: "What's your favorite color",
-    transformer: function(color, answers, flags) {
-      const text = chalkPipe(color)(color);
-      if (flags.isFinal) {
-        return text + '!';
-      }
-
-      return text;
-    }
+    transformer: transformColor
   },
   {
     type: 'input',
     name: 'phone',
     message: "What's your phone number",
-    validate: function(value) {
-      var pass = value.match(
-        /^([01]{1})?[-.\s]?\(?(\d{3})\)?[-.\s]?(\d{3})[-.\s]?(\d{4})\s?((?:#|ext\.?\s?|x\.?\s?){1}(?:\d+)?)?$/i
-      );
-      if (pass) {
-        return true;
-      }
-
-      return 'Please enter a valid phone number';
-    }
+    validate: validatePhoneNumber
   }
-];
+]
 
-inquirer.prompt(questions).then(answers => {
-  console.log(JSON.stringify(answers, null, '  '));
-});
+inquirer.prompt(questions).then(printResults);
