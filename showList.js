023a387fe82fd761ccb9c4273555c72328dcc1ab
Eric Laursen <ericjlaursen@gmail.com>
2019-12-04 05:39:24 -0500

    more functional

--- showList.js
+++ showList.js
@@ -6,8 +6,26 @@
 var inquirer = require('inquirer');
 
 
+//helper fns
 const logAnswers = answers => console.log(JSON.stringify(answers, null, '  '))
 const filterFn = val => val.toLowerCase();
+
+//data
+const pizzaSizes = ['Jumbo', 'Large', 'Standard', 'Medium', 'Small', 'Micro']
+let separator = new inquirer.Separator()
+let toDoChoices = [
+  'Order a pizza',
+  'Make a reservation',
+  'Ask for opening hours',
+  {
+    name: 'Contact support',
+    disabled: 'Unavailable at this time'
+  },
+  'Talk to the receptionist'
+]
+
+//add a line-separator
+toDoChoices.splice(2, 0, separator)
 
 inquirer
   .prompt([
@@ -15,23 +33,13 @@ inquirer
       type: 'list',
       name: 'theme',
       message: 'What do you want to do?',
-      choices: [
-        'Order a pizza',
-        'Make a reservation',
-        new inquirer.Separator(),
-        'Ask for opening hours',
-        {
-          name: 'Contact support',
-          disabled: 'Unavailable at this time'
-        },
-        'Talk to the receptionist'
-      ]
+      choices: toDoChoices
     },
     {
       type: 'list',
       name: 'size',
       message: 'What size do you need?',
-      choices: ['Jumbo', 'Large', 'Standard', 'Medium', 'Small', 'Micro'],
+      choices: pizzaSizes,
       filter: filterFn
     }
   ])
