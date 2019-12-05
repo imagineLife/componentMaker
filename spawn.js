d328887a9cea3b83e5a4aa304ec0c2e2c016cae9
Eric Laursen <ericjlaursen@gmail.com>
2019-12-05 06:20:51 -0500

    removes logs

--- spawn.js
+++ spawn.js
@@ -1,9 +1,6 @@
 var spawn = require('child_process').spawn;
 let srcFile = './input.js'
 
-console.log('spawn')
-console.log(spawn)
-
 spawn('node', [srcFile], {
   cwd: __dirname,
   stdio: 'inherit'
