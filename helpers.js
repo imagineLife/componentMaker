1c0f300d82a148a2fbd1f17d159f983f46f913df
Eric Laursen <ericjlaursen@gmail.com>
2019-12-05 06:20:37 -0500

    raw helpers

---
+++ helpers.js
@@ -0,0 +1,37 @@
+const hzLine = function(){
+
+  // Get the available screen size
+  var width = process.stdout.columns;
+
+  // Put in enough dashes to go across the screen
+  var line = '';
+  for (i = 0; i < width; i++) {
+      line+='-';
+  }
+  console.log(line);
+}
+
+const verticalSpace = function(lineCount){
+  lineCount = typeof(lineCount) == 'number' && lineCount > 0 ? lineCount : 1;
+  for (i = 0; i < lineCount; i++) {
+      console.log('');
+  }
+}
+
+const logTheData = obj => {
+  for(var key in obj){
+     if(obj.hasOwnProperty(key)){
+        var value = obj[key];
+        var line = '      \x1b[33m '+key+'      \x1b[0m';
+        var padding = 60 - line.length;
+        for (i = 0; i < padding; i++) {
+            line+=' ';
+        }
+        line+=value;
+        console.log(line);
+        verticalSpace();
+     }
+  }
+}
+
+module.exports = {hzLine, verticalSpace}
