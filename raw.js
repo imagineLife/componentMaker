57fcbb54f407386c547e1c2864ee6538a4367ebf
Eric Laursen <ericjlaursen@gmail.com>
2019-12-05 20:30:06 -0500

    makes index, css && js files

--- raw.js
+++ raw.js
@@ -36,7 +36,7 @@ const events = require('events');
 class _events extends events{};
 const e = new _events();
 
-const { hzLine, verticalSpace, logTheData, compFromName } = require('./helpers')
+const { hzLine, verticalSpace, logTheData, compFromName, indexFromString } = require('./helpers')
 
 const makeHeader = (str) => {
 	hzLine();
@@ -191,6 +191,10 @@ cli.responders.moreUserInfo = function(str){
 
 // More order info
 cli.responders.makeComponentFiles = function(str){
+
+  console.log('process.argv')
+  console.log(process.argv)
+  
   
   //get ID from string
   let strArr = str.split(' ')
@@ -200,13 +204,14 @@ cli.responders.makeComponentFiles = function(str){
       console.log('err')
       console.log(err)
     }else{
+      //create Component.js file
       fs.open(`${__dirname}/${componentName}/${componentName}.js`,'a', (err, fileDescriptor) => {
 
         if(err || !fileDescriptor){
           return callback('Couldnt open file for appending')
         }
 
-        let componentString = compFromName(componentString)
+        let componentString = compFromName(componentName)
 
         //Append to file and close the file
         fs.appendFile(fileDescriptor,`${componentString}\n`, err => {
@@ -221,8 +226,40 @@ cli.responders.makeComponentFiles = function(str){
               console.log(err)
               return
             }
-            console.log('DONE!');
-            return false
+
+            // create css file
+            fs.open(`${__dirname}/${componentName}/${componentName}.css`,'a', (err, fileDescriptor) => {
+
+              if(err || !fileDescriptor){
+                return callback('Couldnt open file for appending')
+              }
+
+              // create index.js file
+              fs.open(`${__dirname}/${componentName}/index.js`,'a', (err, indexFileDescriptor) => {
+
+                if(err || !indexFileDescriptor){
+                  return callback('Couldnt open file for appending')
+                }
+
+                //Append to file and close the file
+                fs.appendFile(indexFileDescriptor,`${indexFromString(componentName)}\n`, err => {
+                  if(err){
+                    return callback('error appending and closing the file')
+                  }
+
+                  //close the file
+                  fs.close(indexFileDescriptor, (err) => {
+                    if(err){
+                      console.log('ERROR');
+                      console.log(err)
+                      return
+                    }
+
+                    return;
+                  })
+                })
+              })
+            })
           })
         })
       })
@@ -286,6 +323,9 @@ cli.processInput = function(str){
   }
 };
 
+
+console.log('process.argv[2]')
+console.log(process.argv[2])
 // Init logic
 
 //start the cli interface
